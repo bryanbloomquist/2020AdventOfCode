a096a5d2ccce0edd959879250e5d7fd51d112c0a
Bryan Bloomquist <bryan@Bryans-MacBook-Pro.local>
2020-12-11 17:28:51 -0600

    finished up to day 5

---
+++ day.js
@@ -0,0 +1,14 @@
+const data = ``;
+
+const partOne = () => {
+
+    console.log( "Part 1 Solution: " + " " );
+    partTwo();
+}
+
+const partTwo = () => {
+
+    console.log( "Part 2 Solution: " + " " );
+}
+
+partOne();
